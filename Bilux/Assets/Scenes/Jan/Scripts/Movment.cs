5006c28cd1c6ac2c980d61bbd06e7602cfc0e5bc
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-03-08 14:28:59 +0100

    Salto

--- Bilux/Assets/Scenes/Jan/Scripts/Movment.cs
+++ Bilux/Assets/Scenes/Jan/Scripts/Movment.cs
@@ -10,13 +10,20 @@ public class Movment : MonoBehaviour {
     public float jumpForce;
     private Rigidbody2D rb;
     private float moveHorizontal; //sirve para almacenar la posicon horizontal de un joystick o teclado (0 - 1)
-    //private float MoveVertical;
     private float rotationSpeed;
+    private Vector2 speedV2;
+    private Vector2 position; 
+    public LayerMask groundLayer;
+    private float JumpTimeCounter;
+    public float JumpTime;
+    private bool isJumping;
+    
 
 
     void Start () {
 
         rb = GetComponent<Rigidbody2D>();
+       
 	}
 	
 	void Update () {
@@ -27,13 +34,21 @@ public class Movment : MonoBehaviour {
     private void FixedUpdate()
     {
 
+
         moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        speedV2 = rb.velocity;
+        rotationSpeed = rb.angularVelocity;
+        
 
         Move();
 
+        Jump();
+
+        rb.velocity = speedV2;
         rb.angularVelocity = rotationSpeed;
 
-
+        //Debug.Log(IsGorunded());
     }
 
 
@@ -44,39 +59,83 @@ public class Movment : MonoBehaviour {
 
         if (moveHorizontal != 0)
         {
-            if (Mathf.Abs(rb.angularVelocity) < maxVelocity * Mathf.Abs(moveHorizontal)) // acelerar
+            if (Mathf.Abs(rb.angularVelocity) < maxVelocity * Mathf.Abs(moveHorizontal)) 
             {
-                rotationSpeed += acceleration * moveHorizontal * -1;
+                rotationSpeed += acceleration * moveHorizontal * -1; // acelerar
             }
 
-            if(Mathf.Abs(rb.angularVelocity) >= maxVelocity * Mathf.Abs(moveHorizontal))
+            if (Mathf.Abs(rotationSpeed) >= maxVelocity * Mathf.Abs(moveHorizontal))
             {
                 rotationSpeed = maxVelocity * moveHorizontal * -1; //Mover a velocidad constante
             }
 
+            if (((moveHorizontal < 0) && (rb.angularVelocity < 0)) ||   //Permite cambiar de direccion mas rapidamente
+                ((moveHorizontal > 0) && (rb.angularVelocity > 0)))
+            {
+                rotationSpeed += acceleration * moveHorizontal * -1;
+                Debug.Log("lol");
+            }
+
+
         }
-        else if (moveHorizontal == 0)
+        else if (moveHorizontal == 0 && IsGorunded())
         {
             if (rb.angularVelocity > 0)
             {
                 rotationSpeed -= acceleration;
 
-                if(rb.angularVelocity < 0)
+                if (rotationSpeed < 0)
                 {
                     rotationSpeed = 0;
                 }
             }
-            else if(rb.angularVelocity < 0)
+            else if (rb.angularVelocity < 0)
             {
                 rotationSpeed += acceleration;
 
-                if (rb.angularVelocity > 0)
+                if (rotationSpeed > 0)
                 {
                     rotationSpeed = 0;
                 }
             }
         }
+    }
 
-        Debug.Log(rb.angularVelocity);
+    public bool IsGorunded()
+    {
+        position = rb.position;
+        Debug.DrawRay(position, Vector2.down, Color.red, 1, true);
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, 0.5f, groundLayer);
+        
+        if(hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Jump()
+    {
+        if (IsGorunded() && Input.GetButtonDown("Jump"))
+        {
+            speedV2.y = jumpForce;
+            JumpTimeCounter = 0;
+            isJumping = true;
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if(JumpTimeCounter < JumpTime)
+            {
+                speedV2.y = jumpForce;
+                JumpTimeCounter += Time.fixedDeltaTime;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
     }
 }
