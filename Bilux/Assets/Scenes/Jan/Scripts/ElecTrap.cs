68466f22a9d838a04b57aa8117f9b4239fd644a5
Pau <pau.sole@enti.cat>
2019-03-20 12:24:48 +0100

    Alog

---
+++ Bilux/Assets/Scenes/Jan/Scripts/ElecTrap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ElecTrap : MonoBehaviour {
+
+    PMov player;
+    public GameObject ElectricTrap;
+
+    float DOT;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DOT = 1 * Time.deltaTime;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+
+        player = other.gameObject.GetComponent<PMov>();
+        if (player != null)
+        {
+            player.ReceiveDamage(DOT);
+        }
+    }
+}
