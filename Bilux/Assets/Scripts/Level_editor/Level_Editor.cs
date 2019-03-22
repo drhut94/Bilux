03392fe626e515367ee1e5e242da1a20e237af82
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-03-22 14:35:50 +0100

    bloques basicos

--- Bilux/Assets/Scripts/Level_editor/Level_Editor.cs
+++ Bilux/Assets/Scripts/Level_editor/Level_Editor.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 public class Level_Editor : MonoBehaviour {
 
 
-    public GameObject normalBlock, destructibleBlock, destructibleT1;
+    public GameObject normalBlock, destructibleBlock, destructibleT1, hookBlock, bouncyBlock;
     private GameObject block;
     private Vector3 mousePos;
-    public Button normalButton, destructibleButton, destructibleT1Button;
+    public Button normalButton, destructibleButton, destructibleT1Button, hookButton, bouncyButton;
     [HideInInspector]
     public enum BlockName
     {
         destructibleBlock,
         normalBlock,
-        destructibleTriangle1
+        destructibleTriangle1,
+        hooKBlock,
+        bouncyBlock
     }
 
 
@@ -24,8 +26,11 @@ public class Level_Editor : MonoBehaviour {
 
 	void Start ()
     {
+        normalButton.onClick.AddListener(delegate { SelectBlock(BlockName.normalBlock); });
         destructibleButton.onClick.AddListener(delegate { SelectBlock(BlockName.destructibleBlock); });
         destructibleT1Button.onClick.AddListener(delegate { SelectBlock(BlockName.destructibleTriangle1); });
+        hookButton.onClick.AddListener(delegate { SelectBlock(BlockName.hooKBlock); });
+        bouncyButton.onClick.AddListener(delegate { SelectBlock(BlockName.bouncyBlock); });
     }
 
     // Update is called once per frame
@@ -52,6 +57,12 @@ public class Level_Editor : MonoBehaviour {
                 break;
             case BlockName.destructibleTriangle1:
                 block = destructibleT1;
+                break;
+            case BlockName.hooKBlock:
+                block = hookBlock;
+                break;
+            case BlockName.bouncyBlock:
+                block = bouncyBlock;
                 break;
         }
     }
