73a3b1674bf9271ccdde4f49665658c772741a56
Adria Sebastia <adriguiu97@gmail.com>
2019-03-23 18:48:34 +0100

    UI V0.1
    Menu con animación y passa a nivel

---
+++ Bilux/Assets/Scripts/UI/UIInteractions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIInteractions : MonoBehaviour {
+
+    public new Camera camera;
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level 2", LoadSceneMode.Single);
+    }
+    public void Settings()
+    {
+        float posX = camera.transform.position.x;
+
+        posX = 1924;
+        Debug.Log(posX + "camera");
+    }
+}
