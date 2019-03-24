7d91bbe27cf0936627506003657ee2a4b507f24e
Adria Sebastia <adriguiu97@gmail.com>
2019-03-24 22:33:43 +0100

    UI V0.3 Movimiento Camera Incorrecto
    Interacciones UI Correctas.

--- Bilux/Assets/Scripts/UI/UIInteractions.cs
+++ Bilux/Assets/Scripts/UI/UIInteractions.cs
@@ -9,12 +9,18 @@ public class UIInteractions : MonoBehaviour {
     {
         SceneManager.LoadScene("Level 2", LoadSceneMode.Single);
     }
+
     public void Settings(Transform settingsTrans)
     {
         Camera.main.transform.LookAt(settingsTrans.position);
     }
 
-    public void BackButtonMenu(Transform menuTrans)
+    public void Selector(Transform selectorTrans)
+    {
+        Camera.main.transform.LookAt(selectorTrans.position);
+    }
+
+    public void BackMenu(Transform menuTrans)
     {
         Camera.main.transform.LookAt(menuTrans.position);
     }
@@ -22,6 +28,11 @@ public class UIInteractions : MonoBehaviour {
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadEditor()
+    {
+        SceneManager.LoadScene("demo", LoadSceneMode.Single);
     }
 }   
 
