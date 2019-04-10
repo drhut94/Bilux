88d2c5ba9dc141b9836eb5a7e45492163c9ff56e
Adria Sebastia <adriguiu97@gmail.com>
2019-04-10 14:32:04 +0200

    UI amb nivells

--- Bilux/Assets/Scripts/UI/UIInteractions.cs
+++ Bilux/Assets/Scripts/UI/UIInteractions.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class UIInteractions : MonoBehaviour {
 
+    public AudioMixer mixer;
     public GameObject menu;
-    public void LoadLevel()
+    public string group;
+
+    public void Slider(Slider slider)
+    {
+        slider.value = slider.maxValue;
+
+        slider.onValueChanged.AddListener((float u) => SetVolume(u, slider));
+    }
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    public void LoadLevel2()
     {
         SceneManager.LoadScene("Level 2", LoadSceneMode.Single);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3", LoadSceneMode.Single);
     }
 
     public void Settings(GameObject settings)
@@ -37,6 +58,16 @@ public class UIInteractions : MonoBehaviour {
     public void LoadEditor()
     {
         SceneManager.LoadScene("demo", LoadSceneMode.Single);
+    }
+
+    void SetVolume(float value, Slider slider)
+    {
+        mixer.SetFloat(mixer., ConvertToDecibel(value / slider.maxValue));
+    }
+
+    float ConvertToDecibel(float value)
+    {
+        return Mathf.Log10(Mathf.Max(value, 0.0001f)) * 20f;
     }
 }   
 
