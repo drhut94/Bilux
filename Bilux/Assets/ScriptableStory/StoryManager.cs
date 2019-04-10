18ad1852f6248ea1adba16518c5993b2997251cc
Pau <pau.sole@enti.cat>
2019-04-10 21:09:40 +0200

    ScriptableObject story

---
+++ Bilux/Assets/ScriptableStory/StoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Part", menuName = "ScriptableObjects/StoryPart", order = 1)]
+public class StoryManager : ScriptableObject
+{
+
+    [TextArea]
+    public string storyText;
+
+    public string StoryText
+    {
+        get { return storyText; }
+    }
+
+}
---
+++ Bilux/Assets/ScriptableStory/TriggerStory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerStory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    [SerializeField]
+    private StoryManager storyPart;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            Debug.Log(storyPart.storyText);
+        //if (collision.gameObject.tag == "Player")
+        //{
+        //}
+    }
+}
