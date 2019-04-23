9345391d3584accc2f0f929b8882fc3c0b3056c6
Pau <pau.sole@enti.cat>
2019-04-23 18:49:37 +0200

    Story
    Story script changes and damage border

--- Bilux/Assets/ScriptableStory/TriggerStory.cs
+++ Bilux/Assets/ScriptableStory/TriggerStory.cs
@@ -1,28 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerStory : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+    public Text text;
 
     [SerializeField]
     private StoryManager storyPart;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-            Debug.Log(storyPart.storyText);
-        //if (collision.gameObject.tag == "Player")
-        //{
-        //}
+        if (collision.gameObject.tag == "Player")
+        {
+            text.gameObject.SetActive(true);
+            text.text = storyPart.storyText;
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(storyPart.storyText));
+        }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        text.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            text.text += letter;
+            yield return null;
+        }
+    }
+
 }
