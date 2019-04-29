d16de88e58769927aab4d2c28188b0ad24f9ad7c
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-04-29 22:05:44 +0200

    Audio_manager

---
+++ Bilux/Assets/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+		for (int i = 0; i < sounds.Length; i++)
+        {
+            sounds[i].source = gameObject.AddComponent<AudioSource>();
+            sounds[i].source.clip = sounds[i].clip;
+
+            sounds[i].source.volume = sounds[i].volume;
+            sounds[i].source.pitch = sounds[i].pitch;
+        }
+	}
+	
+	public void PlaySound (string name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if(sounds[i].name == name)
+            {
+                sounds[i].source.Play();
+            }
+        }
+    }
+}
