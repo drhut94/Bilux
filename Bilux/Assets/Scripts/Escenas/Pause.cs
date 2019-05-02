4894fecc3b90a56660a333dc2cfbf67c605f66cc
Pau <pau.sole@enti.cat>
2019-05-02 20:28:35 +0200

    Scalinng menu, pause and win script

---
+++ Bilux/Assets/Scripts/Escenas/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public Canvas pause;
+
+    public void Exit()
+    {
+        //SceneManager.LoadScene("menu");
+    }
+
+    public void Continue()
+    {
+        if (Time.timeScale == 0f)
+            Time.timeScale = 1f;
+
+        pause.gameObject.SetActive(false);
+
+    }
+    public void Restart()
+    {
+        //Scene scene = SceneManager.GetActiveScene();
+        //SceneManager.LoadScene(scene.name);
+
+        Continue();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1f)
+                Time.timeScale = 0f;
+
+            pause.gameObject.SetActive(true);
+        }
+    }
+}
