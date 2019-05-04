59c20039efdd8569bd53ad591d7e13d6651b4e9b
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-05-05 00:23:00 +0200

    test ilumination + menu

--- Bilux/Assets/Scripts/UI/UIInteractions.cs
+++ Bilux/Assets/Scripts/UI/UIInteractions.cs
@@ -10,51 +10,60 @@ public class UIInteractions : MonoBehaviour {
 
     public void LoadTutorial()
     {
+        FindObjectOfType<AudioManager>().PlaySound("loadlevel");
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 
     public void LoadLevel1()
     {
+        FindObjectOfType<AudioManager>().PlaySound("loadlevel");
         SceneManager.LoadScene(2, LoadSceneMode.Single);
     }
 
     public void LoadLevel2()
     {
+        FindObjectOfType<AudioManager>().PlaySound("loadlevel");
         SceneManager.LoadScene(3, LoadSceneMode.Single);
     }
 
     public void LoadLevel3()
     {
+        FindObjectOfType<AudioManager>().PlaySound("loadlevel");
         SceneManager.LoadScene(4, LoadSceneMode.Single);
     }
 
 
     public void Settings(GameObject settings)
     {
+        FindObjectOfType<AudioManager>().PlaySound("button");
         settings.SetActive(true);
         menu.SetActive(false);
     }
 
     public void Selector(GameObject selector)
     {
+        FindObjectOfType<AudioManager>().PlaySound("button");
         selector.SetActive(true);
         menu.SetActive(false);
     }
 
     public void SelectorBack(GameObject selector)
     {
+        FindObjectOfType<AudioManager>().PlaySound("button");
         selector.SetActive(false);
         menu.SetActive(true);
     }
 
     public void BackMenu(GameObject UI)
     {
+        FindObjectOfType<AudioManager>().PlaySound("button");
         UI.SetActive(false);
         menu.SetActive(true);
     }
 
     public void QuitGame()
     {
+        FindObjectOfType<AudioManager>().PlaySound("button");
         Application.Quit();
     }
 
