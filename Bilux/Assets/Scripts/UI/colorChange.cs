5bfb051ad7689e015e841ef77415de3033d89836
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-05-08 10:49:26 +0200

    menu improve

---
+++ Bilux/Assets/Scripts/UI/colorChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour {
+
+
+    public Image image;
+    public float speed;
+    private float speedBackup;
+    private Color color;
+    private int r;
+    private int g;
+    private int b;
+
+    private void Start()
+    {
+        speed = speed / 100;
+        speedBackup = speed;
+        r = 0;
+        g = 0;
+        b = 0;
+    }
+
+    private void Update()
+    {
+
+        speed -= Time.deltaTime;
+
+        if (speed <= 0)
+        {
+            if (r < 255)
+            {
+                r++;
+            }
+        }
+
+        image.color = color;
+    }
+
+}
